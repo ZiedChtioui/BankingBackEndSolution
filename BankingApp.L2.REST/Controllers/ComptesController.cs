f92d0edc594334f8ee1d73918b3bf1f7271d8282
Zied Chtioui <Zied.Chtioui@se.linedata.com>
2018-10-31 14:28:21 +0100

    Terminaison de l'affichage de la liste des comptes

---
+++ BankingApp.L2.REST/Controllers/ComptesController.cs
@@ -0,0 +1,22 @@
+﻿using BankingApp.L0.DataAccess.Domain;
+using BankingApp.L1.Business.Facade;
+using BankingApp.L1.Business.Implementations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BankingApp.L2.REST.Controllers
+{
+    public class ComptesController : ApiController
+    {
+        private IComptesBusiness business = new CompteBusinessImpl();
+        // GET api/comptes
+        public IEnumerable<Compte> Get()
+        {
+            return business.TousLesComptes();
+        }
+    }
+}
