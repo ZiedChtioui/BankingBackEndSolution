63d7c86d11ff1fe30c3895ff5b329f6f3ee51294
Zied Chtioui <Zied.Chtioui@se.linedata.com>
2018-10-31 13:54:35 +0100

    Terminaison de la Couche DataAccess avec ses Tests

---
+++ BankingApp.L0.DataAccess/Domain/Compte.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingApp.L0.DataAccess.Domain
+{
+    public class Compte
+    {
+        [Key]
+        public string  Numero { get; set; }
+
+        public string Proprietaire { get; set; }
+
+        public decimal Solde { get; set; }
+
+        public void Debiter(decimal montant)
+        {
+            Solde += montant;
+        }
+
+        public void Crediter(decimal montant)
+        {
+            Solde -= montant;
+        }
+
+        // override object.Equals
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+            Compte converti = obj as Compte;
+
+            return Numero.Equals(converti.Numero);
+        }
+
+        // override object.GetHashCode
+        public override int GetHashCode()
+        {
+            return Numero.GetHashCode();
+        }
+    }
+}
---
+++ BankingApp.L0.DataAccess/Migrations/Configuration.cs
@@ -0,0 +1,27 @@
+namespace BankingApp.L0.DataAccess.Migrations
+{
+    using BankingApp.L0.DataAccess.Domain;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<BankingApp.L0.DataAccess.Infrastructure.BankingContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(BankingApp.L0.DataAccess.Infrastructure.BankingContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+            context.Comptes.AddOrUpdate(new Compte { Numero = "C1000", Proprietaire = "Admin", Solde = 1000M  });
+            context.Comptes.AddOrUpdate(new Compte { Numero = "C2000", Proprietaire = "Krifa", Solde = 13000M });
+            context.Comptes.AddOrUpdate(new Compte { Numero = "C3000", Proprietaire = "Chtioui", Solde = 5000M });
+        }
+    }
+}
